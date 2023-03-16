68ceb84c03818adb6d381b9d9e618bd7a2a84e7e
Guilherme Carvalho <guilherme.santos536@etec.sp.gov.br>
2023-03-15 22:31:34 -0300

    Adicionada lógica de alteração de imagens

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -16,9 +17,34 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener(){
             //gera uma Toast na tela informado que rolamos um dado.
             val toast = Toast.makeText(this, "Rolou um dado!", Toast.LENGTH_SHORT)
-            val resultText: TextView = findViewById(R.id.txtRollResult)
-            resultText.text = "1"
+            rollDice()
         }
     }
+    fun rollDice(){
+        val dice = Dice(6)
+        val resultText: TextView = findViewById(R.id.txtRollResult)
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val diceRoll = dice.roll()
+
+
+
+        if (diceRoll == 1){
+            imageView.setImageResource(R.drawable.dice_1)
+        }
+        if (diceRoll == 2){
+            imageView.setImageResource(R.drawable.dice_1)
+        }
+        if (diceRoll == 3){
+            imageView.setImageResource(R.drawable.dice_1)
+        }
+        resultText.text = diceRoll.toString()
+    }
+
+    class Dice(private val numSides: Int){
+        fun roll(): Int{
+            return (1..numSides).random()
+        }
+    }
+
 
 }
