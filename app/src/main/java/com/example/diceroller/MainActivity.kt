5d9f7183fdc69f5cb542d12e107d48ce1980d0bd
Guilherme Carvalho <guilherme.santos536@etec.sp.gov.br>
2023-03-15 21:20:15 -0300

    [Commit inicial] Projeto Diceroller - Configuração inicial do projeto e layout

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton = findViewById<Button>(R.id.btnRollDice)
+
+        //Define uma função para ser executada quando clicarmos no botão
+        rollButton.setOnClickListener(){
+            //gera uma Toast na tela informado que rolamos um dado.
+            val toast = Toast.makeText(this, "Rolou um dado!", Toast.LENGTH_SHORT)
+            val resultText: TextView = findViewById(R.id.txtRollResult)
+            resultText.text = "6"
+        }
+    }
+
+}
